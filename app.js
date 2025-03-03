9f7c778ab5d9997a119235f1ba762272cd92ce21
karankhosla12 <80817280+karankhosla12@users.noreply.github.com>
2025-03-03 20:35:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -524,7 +524,7 @@ async function filterTasks() {
         
         if (userId) {
             let url = '/assigned';
-            params.push(`/${userId}`);
+            url = url+(`/${userId}`);
         }
         
         if (params.length > 0) {
